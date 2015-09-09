046d03f81cce681d90c90420a99ab340f91751f4
Cris <rhapsodyshadow@yahoo.com>
2015-09-09 15:54:44 -0700

    helper for edit if owner post

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,5 +1,9 @@
-// the function helper to create a unique url for each post
 Template.postItem.helpers({
+  // helper to allow edit if it is owner's post
+  ownPost: function() {
+    return this.userId == Meteor.userId();
+  },
+// the function helper to create a unique url for each post
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
