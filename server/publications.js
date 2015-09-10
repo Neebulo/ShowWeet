521c15b126ec3254cd9ed85402f5de3717d0dc8c
Cris <rhapsodyshadow@yahoo.com>
2015-09-10 16:51:58 -0700

    publication/subscription for comments, comments relate to a post

--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,15 @@
 Meteor.publish('posts', function() {
   return Posts.find();
 });
-//post comments on server side
+
+//post comments on server side (comment relate to a post)
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
+
+/** refactored for comments publish
 Meteor.publish('comments', function() {
   return Comments.find();
 })
+**/
