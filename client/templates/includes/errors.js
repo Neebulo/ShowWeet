0335ea5c4ab27b0aeaa1baa5987c0334cc06fd38
Cris <rhapsodyshadow@yahoo.com>
2015-09-10 12:30:59 -0700

    clear errors after 3 seconds, client/templates/includes/errors.js

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,10 @@ Template.errors.helpers({
     return Errors.find();
   }
 });
+//clear errors after 3 seconds so there is no record in database
+Template.error.onRendered(function() {
+  var error = this.data;
+  Meteor.setTimeout(function () {
+    Errors.remove(error._id);
+  }, 3000);
+});
