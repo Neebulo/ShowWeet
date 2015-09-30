44b3ee8a35331539a6611b5a99aa14dedb2b3339
Cris <rhapsodyshadow@yahoo.com>
2015-09-30 13:01:18 -0700

    Augment postList route to take a limit, broken comment page occurs

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,17 @@
 // to post on server side
+
+Meteor.publish('posts', function(options) {
+  check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Posts.find({}, options);
+});
+/* refactor for Augment
 Meteor.publish('posts', function() {
   return Posts.find();
 });
+*/
 
 //post comments on server side (comment relate to a post)
 Meteor.publish('comments', function(postId) {
@@ -11,8 +21,8 @@ Meteor.publish('comments', function(postId) {
 
 //post notifications server side
 Meteor.publish('notifications', function() {
-  return Notifications.find();
-})
+  return Notifications.find({userId: this.userId, read: false});
+});
 
 /** refactored for comments publish
 Meteor.publish('comments', function() {
