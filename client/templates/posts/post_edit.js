21cee1b190b953a515b31c36085ab143c9f99d74
Cris <rhapsodyshadow@yahoo.com>
2015-10-01 15:03:44 -0700

    added routes for post lists and pages to display, listing is broken

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -49,7 +49,7 @@ if (errors.title || errors.url || errors.description)
     if (confirm("Delete this post?")) {
       var currentPostId = this._id;
       Posts.remove(currentPostId);
-      Router.go('postsList');
+      Router.go('home');
     }
   }
 });
