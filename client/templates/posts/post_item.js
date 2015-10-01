04426f9102df8192f9d79ea832a06869a3df80b2
Cris <rhapsodyshadow@yahoo.com>
2015-10-01 02:18:00 -0700

    grey out upvote if voted or not logged in

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -8,11 +8,20 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
-  }
-});
+  },
+  //disable upvote if not logged in or already voted
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+    } else {
+      return 'disabled';
+    }
+  } //upvotedClass end
+}); //helpers end
 // event listener for upvote
 Template.postItem.events({
-  'click .upvote': function(e) {
+  'click .upvotable': function(e) {
     e.preventDefault();
     Meteor.call('upvote', this._id);
   }
